c47447adb758260676cefa2362468ad36bc13887
Jeff Ahmed (Unifyed) <juned_ahmed@unifyed.com>
2018-08-06 11:08:26 -0400

    first commit

---
+++ src/main/java/test/testController.java
@@ -0,0 +1,15 @@
+package test;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController(value="/")
+public class testController {
+
+	@RequestMapping(value="/home")
+	public  String returnTest(){
+		return "running";
+	}
+}
